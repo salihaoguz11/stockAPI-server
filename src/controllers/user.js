0b7d7635e389bcdc8226a74645693137e769de69
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-04-16 10:14:33 +0100

    user controller completed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,154 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// User Controller:
+const User = require("../models/user");
+const Token = require("../models/token");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+modeul.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+
+    // only can see own fields
+    //admin ise filtreleme yapma am,a degilse sadcee kendi verilerini goster
+    const customFilters = req.user?.isAdmin ? {} : { _id: req.user._id };
+
+    const data = await res.getModelList(User, customFilters);
+
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(User, customFilters),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+
+    // Yeni kayıtlarda admin/staff = false
+    //kimse kendini staff yada admin olarak kayit edemez.
+    req.body.isStaff = false;
+    req.body.isAdmin = false;
+
+    const data = await User.create(req.body);
+
+    /* AUTO LOGIN */
+    const tokenData = await Token.create({
+      userId: data._id,
+      token: passwordEncrypt(data._id + Date.now()),
+    });
+
+    /* AUTO LOGIN */
+    res.status(201).send({
+      error: false,
+      token: tokenData.token,
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+    //? _ olmayan id string dir.   _id ise objecttir.Ikisininde verdigi deger aynidir.
+    // Sadece kendi kaydını görebilir:
+    const customFilters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+
+    // const data = await User.findOne({ _id: req.params.id })
+    const data = await User.findOne(customFilters);
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+    // Sadece kendi kaydını güncelleyebilir:
+    const customFilters = req.user?.isAdmin
+      ? { _id: req.params.id }
+      : { _id: req.user._id };
+
+    // Yeni kayıtlarda admin/staff durumunu değiştiremez: Oldugu gibi kalir.
+    //Mesela staff false dersek uygun olmaz cunku bu kisi staff tir. Verilerin degismesini sadece admin yapar.
+    if (!req.user?.isAdmin) {
+      delete req.body.isStaff;
+      delete req.body.isAdmin;
+    }
+
+    const data = await User.updateOne(customeFilters, req.body, {
+      runValidators: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      data,
+      new: await User.findOne(customFilters),
+    });
+  },
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
+
+    // Permission tarafında permissions.isAdmin kontrolü yapıldığı için burda gerek kalmadı.
+    if (req.params.id != req.user._id) {
+      const data = await User.deleteOne({ _id: req.params.id });
+
+      res.status(data.deletedCount ? 204 : 404).send({
+        error: !data.deletedCount,
+        data,
+      });
+    } else {
+      // Admin kendini silemez.
+      res.errorStatusCode = 403;
+      throw new Error("You can not remove your account.");
+    }
+  },
+};
