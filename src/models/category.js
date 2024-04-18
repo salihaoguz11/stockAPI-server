2fc4c65f86df365dc883591a63e7518542d2c039
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2024-04-18 15:13:21 +0100

    models completed

--- src/models/category.js
+++ src/models/category.js
@@ -1,6 +1,26 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Category Model:
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      set: (name) => name.toUpperCase(),
+    },
+  },
+  {
+    collection: "categories",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Category", CategorySchema);
